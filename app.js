17e2b8c8abf916ba39568713cd4c49917ac973b7
Edward Leung <edward_leung_12@hotmail.com>
2020-10-25 19:31:08 +0000

    user can pick the winningscore in game

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const p2Button = document.querySelector('#p2Button');
 const resetButton = document.querySelector('#reset');
 const p1Display = document.querySelector('#p1Display');
 const p2Display = document.querySelector('#p2Display');
+const winningScoreSelect = document.querySelector('#playto');
 
 let p1Score = 0;
 let p2Score = 0;
 let winningScore = 5;
 let isGameOver = false;
 
+// Player 1 Button
 p1Button.addEventListener('click', function() {
     if (!isGameOver) {
         p1Score += 1;
@@ -18,6 +20,8 @@ p1Button.addEventListener('click', function() {
         p1Display.textContent = p1Score;
     }
 });
+
+// Player 2 Button
 p2Button.addEventListener('click', function() {
     if (!isGameOver) {
         p2Score += 1;
@@ -28,10 +32,20 @@ p2Button.addEventListener('click', function() {
     }
 });
 
-resetButton.addEventListener('click', function() {
+// Winning Score Selector
+winningScoreSelect.addEventListener('change', function() {
+    winningScore = parseInt(this.value);
+    reset();
+});
+
+// Reset Button
+resetButton.addEventListener('click', reset) 
+
+// Reset Function
+function reset() {
     isGameOver = false;
     p1Score = 0;
     p2Score = 0;
     p1Display.textContent = 0;
     p2Display.textContent = 0;
-});
+}
